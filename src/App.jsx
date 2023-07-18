709c6e9818c0808a92f47ade8ad713422e769366
Malena Guallar <malena.guallar@gmail.com>
2023-07-18 12:29:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Home} from "./Pages/Home/index.jsx"
+import "./App.css"
+
+
+const App = () => {
+
+  return (
+
+    <>
+    <BrowserRouter>
+    <Routes>
+
+        <Route path="/home" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+};
+
+export default App ;
