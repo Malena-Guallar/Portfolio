f44805c507f1ba7885d2f0d4bcbe396bc1b9ee45
Malena Guallar <malena.guallar@gmail.com>
2023-07-18 16:42:43 +0200

    styling home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -10,7 +10,8 @@ export const Home = () => {
       <div className="text-wrapper-2">FRONT END DEVELOPER ___</div>
       <div className="text-wrapper-3">based in Paris</div>
       <p className="p">hi, welcome to my portfolio</p>
-      <p className="text-wrapper-4">I’m looking for an internship starting in october.</p>
+      <p className="text-wrapper-4">I’m looking for a one year apprenticeship.</p>
+      <p className="text-wrapper-5">I’m flexible ! I can start between october and february.</p>
       <img className="image" alt="Image" src="src/images/image 1.svg" />
       <img className="arrow" alt="Arrow" src="arrow-1.svg" />
     </div>
