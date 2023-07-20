2c74d90d1acb79b44ba833071a3cfaf822e56d02
Malena Guallar <malena.guallar@gmail.com>
2023-07-20 15:45:26 +0200

    adding react use gesture and useref to handle scrolling

--- src/Pages/Global/index.jsx
+++ src/Pages/Global/index.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { React, useRef } from "react";
 import { Home } from "../Home/index";
 import { About } from "../About/index.jsx";
 import { Project_1 } from "../Project_1/index.jsx";
 import { Project_2 } from "../Project_2/index.jsx";
 import { Project_3 } from "../Project_3/index.jsx";
 import { Project_4 } from "../Project_4/index.jsx";
+import { useAnimation } from "framer-motion";
+import { useGesture } from "react-use-gesture";
+
+const pages = [Home, About, Project_1, Project_2, Project_3, Project_4];
+const totalPages = pages.length;
 
 export const Global = () => {
+    const controls = useAnimation();
+    const pageRef = React.useRef(0);
+
+    const whereAmI = useGesture({})
+
 
     return (
         <>
@@ -16,7 +26,7 @@ export const Global = () => {
             <Project_2 />
             <Project_3 />
             <Project_4 />
-            
+
         </>
 
 
