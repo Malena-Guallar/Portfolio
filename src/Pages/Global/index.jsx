4f78d46c9af96be37f85f2c10234152321bb46f5
Malena Guallar <malena.guallar@gmail.com>
2023-07-20 16:11:00 +0200

    handling scrolling

--- src/Pages/Global/index.jsx
+++ src/Pages/Global/index.jsx
@@ -5,29 +5,56 @@ import { Project_1 } from "../Project_1/index.jsx";
 import { Project_2 } from "../Project_2/index.jsx";
 import { Project_3 } from "../Project_3/index.jsx";
 import { Project_4 } from "../Project_4/index.jsx";
-import { useAnimation } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { useGesture } from "react-use-gesture";
 
 const pages = [Home, About, Project_1, Project_2, Project_3, Project_4];
 const totalPages = pages.length;
+const sensitivity = 2;
 
 export const Global = () => {
     const controls = useAnimation();
-    const pageRef = React.useRef(0);
+    const pageRef = useRef(0);
+    const scrollCounter = useRef(0);
 
-    const whereAmI = useGesture({})
+    const whereAmI = useGesture({
+        onWheel: ({ delta: [, deltaY] }) => {
+            scrollCounter.current += Math.sign(deltaY);
+
+            if (scrollCounter.current >= sensitivity && pageRef.current < totalPages - 1) {
+                pageRef.current++;
+                scrollCounter.current = 0;
+            } else if (scrollCounter.current <= -sensitivity && pageRef.current > 0) {
+                pageRef.current--;
+                scrollCounter.current = 0;
+            }
+            scrollToPage(pageRef.current);
+        },
+    });
+
+    const scrollToPage = (pageIndex) => {
+        controls.start({
+            y: `-${pageIndex * 100}vh`,
+            transition: {type: "tween", duration: 0.5 },
+        });
+    };
 
 
     return (
-        <>
-            <Home />
-            <About />
-            <Project_1 />
-            <Project_2 />
-            <Project_3 />
-            <Project_4 />
+        <div {...whereAmI()} style={{ overflow: "hidden"}}>
+            <motion.div
+            style={{ display: "flex", flexDirection: "column" }}
+            animate={controls}
+            >
+                {pages.map((Page, index) => (
+                    <div key={index} style={{minHeight: "100%"}}>
+                        <Page />
+                    </div>
+                ))};
 
-        </>
+            </motion.div>
+
+        </div>
 
 
     )
