04cc70ccf6b371149553f35dc4c347f0bdc4a8ee
Malena Guallar <malena.guallar@gmail.com>
2023-08-06 14:48:15 +0200

    mobile version

--- src/Pages/Footer/index.jsx
+++ src/Pages/Footer/index.jsx
@@ -8,11 +8,11 @@ export const Footer = () => {
             {/* <p className="infos">this app was designed and developed by myself using React with Vite © Malena Guallar 2023</p> */}
             <div className="footer_infos">
                 <p className="nums">❊ 2711</p>
-                {/* <img className="M" src="./images/M.png" /> */}
+                <img className="M" src="./images/M.png" />
                 <p className="name"> &emsp; Malena Guallar © 2023</p> 
                 <br/>
+                <img className="latlong" src="./images/latlong.png" />
                 <p className="location">&emsp; &emsp; Montreuil ✱, </p>
-                {/* <img className="latlong" src="./images/latlong.png" /> */}
                 <p className="location">France 93100</p>
                 <br/>
                 <p className="mail"> &#123; ☎ malena.guallar@gmail.com &#125; </p>
