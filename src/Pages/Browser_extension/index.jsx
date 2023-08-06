2ba0aec429b38d603ced7d7ae911173825885aeb
Malena Guallar <malena.guallar@gmail.com>
2023-08-06 14:51:28 +0200

    mobile version

--- src/Pages/Browser_extension/index.jsx
+++ src/Pages/Browser_extension/index.jsx
@@ -10,8 +10,8 @@ export const Browser_extension = () => {
             titre={"Browser extension"}
             content={
                 <div>
-                    <p>As a group of three persons we decided to create a homepage extension which displays a customed page on every new tab opening. The user finds a welcoming message, the time and weather, and its daily horoscope. </p>
-                    <p>During this project I mostly worked on the global architecture of the extension : creating a form and implementing functions to stock data in browser local storage, then displaying a custom page for each user. I customed the API requests with the data from local storage and the date, to display a horoscope that changes in accordance with the sign of the user, and of course changes everyday. We chose to display a nice design with animation from a SVG code we found on the web on top of some CSS work.</p>
+                    <p>Homepage extension which displays a customed page on every new tab opening. The user finds a welcoming message, the time and weather, and its daily horoscope. </p>
+                    <p>➜ I mostly worked on the global architecture of the extension : creating a form and implementing functions to stock data in browser local storage, then displaying a custom page for each user.</p>
                     <p className="tools">Tools and languages : JavaScript, html, css, svg, Postman</p>
                 </div>}
             img={"./images/browserextension.png"}
--- src/Pages/Dataviz/index.jsx
+++ src/Pages/Dataviz/index.jsx
@@ -10,7 +10,7 @@ export const Dataviz = () => {
             titre={"Dataviz"}
             content={
                 <div className="content">
-                    <p>This project was designed as a group exercise in order to learn how to use API requests to create a dynamic webpage. We used several API in order to render time and weather informations, with a map, centered on Paris' tenth arrondissement, with drawings of the neighbouring parks and green spaces. We used Paris Data and a weather API.</p>
+                    <p>This project was designed as a group exercise in order to learn how to use <b>API requests</b> to create a dynamic webpage. We used several API in order to render time and weather informations, with a map, centered on Paris' tenth arrondissement, with drawings of the neighbouring parks and green spaces. We used Paris Data and a weather API.</p>
                     <p className="tools">Tools and languages we used : JS, Html, Css, Git, Figma, Jest, Postman.</p>
                 </div>}
             img={"./images/dataviz.png"}
