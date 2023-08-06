08adf25a9d14999da5fe7254a66993d02b6fbc29
Malena Guallar <malena.guallar@gmail.com>
2023-08-06 16:41:57 +0200

    working on responsiveness

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -7,23 +7,25 @@ export const About = () => {
 
         <div className="about">
             <img className="waves" src="./images/waves1.png" />
-            <div className="title-wrapper">
-                <img className="star" alt="Image" src="./images/image1.svg" />
-                <h1 className="title">about</h1>
-            </div>
-            <div className="about-text">
-                    <p>Hi there,</p>
-                    <p>I'm Malena, developer apprentice at Ada Tech School since January 2023.</p>
-                    <p>I used to be a cheffe in a restaurant, conceiving and designing courses as well as conceiving and designing apps is something I am passionate about.</p>
-                    <p>I develop with <b>Javascript</b> and am currently learning Java. I like to use tools such as <b>React, Next, Jest</b>. </p>
-                    <p>I love <b>tests and cleancode</b>. </p>
-                    <p>Go check my repositories to learn about all the tools I'm learning.</p>
-                    <p>I'm looking for a one-year apprenticeship starting between October 23 and February 24 - let's get in touch !</p>
-                    <p> ➜ email me at malena.guallar@gmail.com</p>
-                    <p>➜ download my resume <a href="/public/CV.pdf" download="cv_malena_guallar">here</a></p>
+            <div className="div-wrapper">
+                <div className="title-wrapper">
+                    <img className="star" alt="Image" src="./images/image1.svg" />
+                    <h1 className="title">about</h1>
+                </div>
+                <div className="about-text">
+                        <p>Hi there,</p>
+                        <p>I'm Malena, developer apprentice at Ada Tech School since January 2023.</p>
+                        <p>I used to be a cheffe in a restaurant, conceiving and designing courses as well as conceiving and designing apps is something I am passionate about.</p>
+                        <p>I develop with <b>Javascript</b> and am currently learning Java. I like to use tools such as <b>React, Next, Jest</b>. </p>
+                        <p>I love <b>tests and cleancode</b>. </p>
+                        <p>Go check my repositories to learn about all the tools I'm learning.</p>
+                        <p>I'm looking for a one-year apprenticeship starting between October 23 and February 24 - let's get in touch !</p>
+                        <p> ➜ email me at malena.guallar@gmail.com</p>
+                        <p>➜ download my resume <a href="/public/CV.pdf" download="cv_malena_guallar">here</a></p>
+                </div>
             </div>
 
-            <div className="infos_container">
+            <div className="about_infos_container">
                 <div className="tools_container">
                     <img className="star1" src="./images/image1.svg" />
                     <h4 className="tools_title">TOOLS & LANGUAGES</h4>
